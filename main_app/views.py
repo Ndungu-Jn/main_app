59830802be580d396adbdbb1df8e862fd5e80a87
Ndungu-Jn <ndungulink@gmail.com>
2024-11-18 17:29:34 +0300

    templatnig in djnago

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,13 @@ def about(request):
     return render(request, 'about.html')
 
 def services(request):
-    return render(request, 'services.html')    
+    our_services = ["Bush camps", "Ballon Tours", "firecamps", "color festivals"]
+    price = 10000
+    date = '15-11-2024'
+    return render(request, 'services.html', {"services": our_services, "price":price, "date":date})    
+
+
+#dislpay data in our pages
+#loops
+#if statements
+#Templating engine language
